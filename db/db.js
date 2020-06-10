96043ccfb8cfc99b378079114657d11639d55d9c
natEngelking <natalie.mixdorf@gmail.com>
2020-06-09 21:06:12 -0600

    tables are good

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   "user": "gnome",
   "host": "localhost",
-  "database" : "Gnome-Grown",
+  "database" : "gnomegrown",
   "password" : "Gnomeo",
   "port": 3004
 })
